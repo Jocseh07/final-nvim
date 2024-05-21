3994fea2bcac70e535f1a57d9ad52f3dd2ff9777
Jocseh07 <josephmkaranja07@gmail.com>
2024-05-21 07:10:25 +0300

    more

--- lua/community.lua
+++ lua/community.lua
@@ -13,6 +13,14 @@ return {
   { import = "astrocommunity.pack.markdown" },
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.indent.indent-rainbowline" },
+  { import = "astrocommunity.editing-support.auto-save-nvim" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.bars-and-lines.vim-illuminate" },
 
   -- import/override with your plugins folder
 }
